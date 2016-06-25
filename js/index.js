ed4752c1643376d80c3cb1e250303ce9f5c31394
Cubees <cubees.babylon@gmail.com>
2016-06-25 16:22:48 +0100

    scripts added

---
+++ js/index.js
@@ -0,0 +1,84 @@
+(function () {
+
+    // ----------------------
+    // LEFT MENU FOR MOBILE
+    // ----------------------
+
+    window.addEventListener("DOMContentLoaded", function () {
+
+        var createSlideOutMenu = function (callback) {
+            var divSearchBarMobile =
+                $('<div>', {
+                    class: 'searchbar-mobile searchbar',
+                    html: '<form method="get" action="/search"><input type="text" name="q" placeholder="Search..."/>' +
+                    '<button type="submit"><i class="fa fa-search"></i></button></form>'
+                });
+
+             var navigationLinks = [
+                $('<a>', {
+                    href: '/',
+                    html: '<i class="fa fa-home"></i>Home'
+                }),
+                $('<a>', {
+                    href: '/begins',
+                    html: '<i class="fa fa-book"></i>Begin Here'
+                }),
+                $('<a>', {
+                    href: '/basics',
+                    html: '<i class="fa fa-book"></i>Basics'
+                }),
+                $('<a>', {
+                    href: '/intermediate',
+                    html: '<i class="fa fa-book"></i>Intermediate'
+                }),
+                $('<a>', {
+                    href: '/advanced',
+                    html: '<i class="fa fa-book"></i>Advanced'
+                })
+            ];
+
+
+            var links = [
+                $('<a>', {
+                    href: 'http://www.babylonjs.com',
+                    target: '_blank',
+                    html: '<i class="fa fa-play"></i>babylonjs.com'
+                }),
+                $('<a>', {
+                    href: 'https://github.com/BabylonJS/Babylon.js',
+                    target: '_blank',
+                    html: '<i class="fa fa-github"></i>Github'
+                }),
+                $('<a>', {
+                    href: 'https://github.com/BabylonJS/Documentation',
+                    target: '_blank',
+                    html: '<i class="fa fa-code-fork"></i>Contribute'
+                }),
+                $('<a>', {
+                    href: 'http://www.html5gamedevs.com/forum/16-babylonjs',
+                    target: '_blank',
+                    html: '<i class="fa fa-html5"></i>Forum'
+                })
+            ];
+
+            $('#menu').append(divSearchBarMobile)
+                .append(navigationLinks)
+                .append(links);
+
+            callback();
+
+        }(function () {
+            var slideout = new Slideout({
+                'panel': document.getElementById('wrapper'),
+                'menu': document.getElementById('menu'),
+                'padding': 256,
+                'tolerance': 70,
+                'touch': false
+            });
+
+            $("#mobilemenu").click(function () {
+                slideout.toggle();
+            });
+        });
+    });
+})();
